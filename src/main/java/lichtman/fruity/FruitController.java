1ddc49e3f8b93ceab68abf879e6d03e3150b8dbd
Hannah Lichtman <chani.lichtman@gmail.com>
2026-04-29 11:19:50 -0400

    connected unsplash pictures to FruitFrame

--- src/main/java/lichtman/fruity/FruitController.java
+++ src/main/java/lichtman/fruity/FruitController.java
@@ -1,23 +1,27 @@
 package lichtman.fruity;
 
+import com.andrewoid.apikeys.ApiKey;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+import lichtman.fruity.unsplash.Photos;
+import lichtman.fruity.unsplash.UnsplashService;
+import lichtman.fruity.unsplash.UnsplashServiceFactory;
 
 import javax.swing.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class FruitController {
-    private FruityService service;
-    private JLabel picLabel;
-    private JLabel familyInfo;
-    private JLabel orderInfo;
-    private JLabel genusInfo;
-    private JLabel caloriesInfo;
-    private JLabel fatInfo;
-    private JLabel sugarInfo;
-    private JLabel carbsInfo;
-    private JLabel proteinInfo;
+    private final FruityService service;
+    private final JLabel picLabel;
+    private final JLabel familyInfo;
+    private final JLabel orderInfo;
+    private final JLabel genusInfo;
+    private final JLabel caloriesInfo;
+    private final JLabel fatInfo;
+    private final JLabel sugarInfo;
+    private final JLabel carbsInfo;
+    private final JLabel proteinInfo;
 
     public FruitController(
             FruityService service,
@@ -71,11 +75,15 @@ public class FruitController {
         proteinInfo.setText(String.valueOf(nutritions.protein()));
 
         try {
-            ImageIcon imageIcon = new ImageIcon(new URL("https://picsum.photos/800/600"));
-            picLabel.setIcon(imageIcon);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+            UnsplashService service = new UnsplashServiceFactory().create();
+            ApiKey apiKey = new ApiKey();
+            String keyString = apiKey.get();
+            Photos photo = service.search(keyString, fruit.name()).blockingGet();
 
+            ImageIcon imageIcon = new ImageIcon(new URL(photo.results()[0].urls().small()));
+            picLabel.setIcon(imageIcon);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
